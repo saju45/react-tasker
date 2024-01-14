e2a4a3e1ee177d058935258529bbec742b5ed83f
Shanawaj hossain <princesaju645@gmail.com>
2024-01-14 17:09:22 +0600

    add task fearute done

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import AddTaskModal from "./AddTaskModal";
 import Search from "./Search";
 import TaskAction from "./TaskActions";
 import TaskList from "./TaskList";
@@ -15,13 +16,21 @@ export default function TaskBoard() {
     isFavorite: true,
   };
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  const handleAddClick = (data) => {
+    setTasks([...tasks, data]);
+    console.log(data);
+    setShowAddModal(false);
+  };
 
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal onSave={handleAddClick} />}
       <div className="container">
         <Search />
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction />
+          <TaskAction onAddClick={() => setShowAddModal(true)} />
           <TaskList tasks={tasks} />
         </div>
       </div>
